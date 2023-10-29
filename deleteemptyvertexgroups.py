5be25178add6d8b4360861f5896ec5e6adcf8957
moyashi4747 <yhfldtm2130@gmail.com>
2023-10-29 15:19:44 +0900

    initial release

---
+++ deleteemptyvertexgroups.py
@@ -0,0 +1,65 @@
+bl_info = {
+    "name": "Remove Empty Vertex Groups",
+    "blender": (2, 80, 0),
+    "category": "Object",
+}
+
+import bpy
+
+class OBJECT_OT_remove_empty_vertex_groups(bpy.types.Operator):
+    bl_idname = "object.remove_empty_vertex_groups"
+    bl_label = "Remove Empty Vertex Groups"
+    bl_description = "Remove all vertex groups with no weights assigned to any vertex"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        obj = context.active_object
+        
+        if obj is None or obj.type != 'MESH':
+            self.report({'ERROR'}, "No active mesh object selected")
+            return {'CANCELLED'}
+        
+        to_remove = []
+        
+        for vg in obj.vertex_groups:
+            group_id = vg.index
+            for v in obj.data.vertices:
+                try:
+                    if vg.weight(v.index) > 0:
+                        break
+                except RuntimeError:
+                    pass
+            else:
+                to_remove.append(vg.name)
+
+        for vg_name in to_remove:
+            obj.vertex_groups.remove(obj.vertex_groups[vg_name])
+        
+        self.report({'INFO'}, f"Removed {len(to_remove)} empty vertex groups")
+        return {'FINISHED'}
+
+def draw_func(self, context):
+    layout = self.layout
+    layout.operator(OBJECT_OT_remove_empty_vertex_groups.bl_idname)
+
+class VIEW3D_PT_tools_remove_empty_vertex_groups(bpy.types.Panel):
+    bl_label = "Remove Empty Vertex Groups"
+    bl_idname = "OBJECT_PT_remove_empty_vertex_groups"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Tool'
+    bl_context = 'objectmode'
+    
+    def draw(self, context):
+        draw_func(self, context)
+
+def register():
+    bpy.utils.register_class(OBJECT_OT_remove_empty_vertex_groups)
+    bpy.utils.register_class(VIEW3D_PT_tools_remove_empty_vertex_groups)
+
+def unregister():
+    bpy.utils.unregister_class(OBJECT_OT_remove_empty_vertex_groups)
+    bpy.utils.unregister_class(VIEW3D_PT_tools_remove_empty_vertex_groups)
+
+if __name__ == "__main__":
+    register()
